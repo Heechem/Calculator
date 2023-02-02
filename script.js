914351b152541b7071cd061779926f610caf028f
Heechem <hichempeter@gmail.com>
2023-02-01 18:38:19 -0700

    add the display

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 "use strict";
+const buttons = document.querySelectorAll(".button");
+const screen = document.querySelector(".screen");
+const wrapper = document.querySelector(".wrapper");
+const numbers = document.querySelectorAll("[data-input]");
+const erase = document.getElementById("erase");
+console.log(numbers);
 
 class Operator {
   constructor(a, b) {
@@ -22,3 +28,28 @@ class Operator {
 const calculation = new Operator();
 console.log(calculation.addition(5, 12), calculation.substract(10, 5));
 console.log(2);
+
+// wrapper.addEventListener("click", function () {
+//   numbers.forEach((nbr) => {
+//     if (nbr.dataset["data-input"]) {
+//       screen.innerHTML = nbr.attributes();
+//     }
+//     console.log(nbr);
+//   });
+// });
+let selection = [];
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.getAttribute("data-input")) {
+      selection.push(btn.getAttribute("data-input"));
+    }
+    console.log(selection);
+    screen.innerHTML = Number(selection.join(""));
+  });
+});
+
+erase.addEventListener("click", () => {
+  screen.innerHTML = "";
+  selection = [];
+});
