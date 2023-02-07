50a5ee24c2810538bf66633852b19c21093473a4
Heechem <hichempeter@gmail.com>
2023-02-06 17:46:16 -0700

    fix the computation

--- READMe.md
+++ READMe.md
@@ -2,3 +2,5 @@ Creating the UI
 Adding the display
 Adding the function
 Store the value into an array
+computation with with storage of the first value added
+bug durring the next computation (calculating only the first operation)
--- script.js
+++ script.js
@@ -9,9 +9,13 @@ const decimalPoint = document.getElementById(".");
 const addBtn = document.getElementById("add");
 const operationsWrapper = document.querySelector(".operations");
 const equal = document.getElementById("equal");
+const ops = document.querySelectorAll(".ops");
+const subBtn = document.getElementById("sub");
+const divBtn = document.getElementById("divide");
+const multiBtn = document.getElementById("multiply");
 
 let selection1 = [];
-let savedNumbers = [];
+let savedNumbers;
 let opsRecord;
 let selection2 = [];
 let results;
@@ -23,9 +27,8 @@ class Operator {
     this.a = a;
     this.b = b;
 
-    this.eraser();
-    this.opsfind();
-    this.calculationProcess();
+    this.handlClick();
+    this.compute();
   }
   addition(a, b) {
     return a + b;
@@ -49,31 +52,13 @@ class Operator {
 
   // enable the eraser function C
   eraser() {
-    erase.addEventListener("click", () => {
-      screen.innerHTML = "";
-      selection1 = [];
-      decimalPoint.classList.remove("disabled");
-      savedNumbers = [];
-      opsRecord;
-      selection2 = [];
-    });
-  }
-
-  // The operators records
-  opsfind() {
-    operationsWrapper.addEventListener("click", (e) => {
-      let id = e.target.id;
-      if (
-        id === "add" ||
-        id === "sub" ||
-        id === "multiply" ||
-        id === "divide"
-      ) {
-        this.opsRecord = id;
-      }
-
-      console.log(this.opsRecord);
-    });
+    screen.innerHTML = "";
+    decimalPoint.classList.remove("disabled");
+    savedNumbers;
+    opsRecord = undefined;
+    selection1 = [];
+    selection2 = [];
+    this.results = "";
   }
 
   // computation process
@@ -83,30 +68,62 @@ class Operator {
     previousOperand = parseFloat(selection2.join(""));
     currentOperand = parseFloat(selection1.join(""));
     if (isNaN(previousOperand) || isNaN(currentOperand)) return;
-    console.log(this.opsRecord);
-    switch (this.opsRecord) {
+    console.log(opsRecord);
+    switch (opsRecord) {
       case "add":
         this.results = this.addition(previousOperand, currentOperand);
+
         break;
       case "sub":
         this.results = this.substract(previousOperand, currentOperand);
+
         break;
       case "multiply":
         this.results = this.multiply(previousOperand, currentOperand);
+
         break;
       case "divide":
         this.results = this.divide(previousOperand, currentOperand);
+
         break;
     }
-    console.log(selection1, selection2);
-    console.log(previousOperand, currentOperand);
+    console.log(selection2, selection1, previousOperand, currentOperand);
+
     screen.innerHTML = `${this.results}`;
-    console.log(this.results);
   }
   nextStep() {
-    console.log(this.results);
+    // if (this.results === undefined) return;
+    this.opsRecord = "";
+
     selection1 = [];
     selection1.push(this.results);
+    console.log(this.results);
+  }
+  compute() {
+    equal.addEventListener("click", () => {
+      this.calculationProcess();
+      this.nextStep();
+      console.log(results);
+    });
+
+    addBtn.addEventListener("click", () => {
+      this.calculationProcess();
+      this.nextStep();
+    });
+    // subBtn.addEventListener("click", () => {
+    //   this.calculationProcess();
+    //   this.nextStep();
+    // });
+    // multiBtn.addEventListener("click", () => {
+    //   this.calculationProcess();
+    //   this.nextStep();
+    //   console.log(results);
+    // });
+    // divBtn.addEventListener("click", () => {
+    //   this.calculationProcess();
+    //   this.nextStep();
+    //   console.log(results);
+    // });
   }
 }
 
@@ -115,43 +132,65 @@ const calculation = new Operator();
 // the display function
 
 buttons.forEach((btn) => {
-  if (selection1.length === 0) {
-    btn.addEventListener("click", () => {
-      if (btn.getAttribute("data-input")) {
-        selection1.push(btn.getAttribute("data-input"));
-        screen.innerHTML = Number(selection1.join(""));
-      }
-    });
-  } else selection2.push(selection1);
-});
-
-// store the first and second value
-
-operators.forEach((ops) => {
-  ops.addEventListener("click", () => {
-    if (selection2.length === 0) {
-      selection2.push(selection1.join(""));
-
-      selection1 = [];
-
-      screen.innerHTML = "";
-    } else {
-      selection2.splice(0);
-      selection2.push(selection1.join(""));
-      selection1 = [];
-      screen.innerHTML = "";
-    }
-    if (decimalPoint.classList.contains("disabled")) {
-      decimalPoint.classList.remove("disabled");
+  btn.addEventListener("click", () => {
+    if (btn.getAttribute("data-input")) {
+      selection1.push(btn.getAttribute("data-input"));
+      screen.innerHTML = Number(selection1.join(""));
     }
   });
 });
 
+function firstStep() {
+  //get the operator
+  operationsWrapper.addEventListener("click", (e) => {
+    if (e.target.id) {
+      let id = e.target.id;
+      if (
+        id === "add" ||
+        id === "sub" ||
+        id === "multiply" ||
+        id === "divide"
+      ) {
+        opsRecord = id;
+      }
+      // get the second number
+      if (selection2.length === 0) {
+        selection2.push(selection1.join(""));
+
+        selection1 = [];
+
+        screen.innerHTML = "";
+      } else {
+        selection2.splice(0);
+        selection2.push(selection1.join(""));
+        selection1 = [];
+        screen.innerHTML = "";
+      }
+      if (decimalPoint.classList.contains("disabled")) {
+        decimalPoint.classList.remove("disabled");
+      }
+    }
+  });
+}
+
+firstStep();
+
 // calculate the result
 
-equal.addEventListener("click", (button) => {
-  calculation.calculationProcess();
+// equal.addEventListener("click", () => {
+//   calculation.calculationProcess();
+//   calculation.nextStep();
+//   console.log(results);
+// });
 
-  calculation.nextStep();
-  console.log(selection1);
+erase.addEventListener("click", () => {
+  calculation.eraser();
 });
+
+// operationsWrapper.addEventListener("click", (e) => {
+//   let id = e.target.id;
+//   if (id === "add" || id === "sub" || id === "multiply" || id === "divide") {
+//     opsRecord = id;
+//     console.log(111);
+//   }
+// });
